48ee13bd388484450bafe005b7c7d7e8585301e4
ayan412 <mussatayev.ayan@gmail.com>
2024-03-28 19:47:54 +0500

    reading first rows of the file

--- task4/main.go
+++ task4/main.go
@@ -1,3 +1,40 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	filePath       string = "./63_4/"   // Каталог, содержащий входные файлы
+	outputFilePath string = "./output/" // Каталог, в который будут записываться выходные файлы
+)
+
+
+func main() {
+	// Переменная для хранения пути к текущему файлу
+	var filePathFull string
+
+	// Цикл для обработки каждого файла в каталоге "59_3"
+	for i:=1;i<=1;i++{
+		iStr := strconv.Itoa(i)
+		// Путь до файла
+		filePathFull = fmt.Sprintf("%s%s", filePath, iStr)
+
+		//Открытие файла
+		f, err := os.Open(filePathFull)
+		if err != nil {
+			fmt.Println("Error while opening the file", err)
+			break
+		}
+		defer f.Close() // Закрытие файла по окончании работы с ним
+
+		//Создание Reader для чтения файла
+		rdr := bufio.NewReader(f)
+		
+	}
+}
 
 
