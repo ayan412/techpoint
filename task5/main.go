4ac529ed097a0bb40dd14e4e3ce475c5ff15efb1
ayan412 <mussatayev.ayan@gmail.com>
2024-05-20 21:10:50 +0500

    task5 completed

--- task5/main.go
+++ task5/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"strconv"
 	"strings"
 	"time"
@@ -13,7 +15,7 @@ import (
 
 const (
 	filePath       string = "./71_5/"   // Папка, содержащая входные файлы для теста
-	outputFilePath string = "./output/" // Папка в которую будут записываться выходные файлы
+	outputFilePath string = "./output/" // Папка, в которую будут записываться выходные файлы
 )
 
 // Функция для измерения времени выполнения
@@ -27,17 +29,17 @@ func track(msg string) (string, time.Time) {
 }
 
 // Функция для создания выходного файла
-func writeToOutputFile(iter int) *os.File {
+func writeToOutputFile(iter int) (*os.File, error) {
 	// Формирование пути к выходному файлу
 	pathToFile := fmt.Sprintf("%s%d.txt", outputFilePath, iter)
 
 	// Открытие или создание файла
-	outFile, err := os.OpenFile(pathToFile, os.O_WRONLY|os.O_CREATE, 0666)
+	outFile, err := os.OpenFile(pathToFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
-		fmt.Println("failed to open or create file: %w", err)
-		return nil
+		fmt.Println("failed to open or create file:", err)
+		return nil, err
 	}
-	return outFile
+	return outFile, nil
 }
 
 type FoldersPath struct {
@@ -47,117 +49,136 @@ type FoldersPath struct {
 }
 
 func main() {
-
-	numSets, rdr, _ := readSets()
-
-	for i := 1; i <= numSets; i++ {
-		numRows := readRows(rdr)
-		readJson(numRows, rdr)
-
+	// Профилирование памяти
+	f, err := os.Create("memprofile.prof")
+	if err != nil {
+		fmt.Println("Не удалось создать файл профиля:", err)
+		return
 	}
-	// for i := 2; i <= numSets; i++ {
-	// 	numRows := readRows(rdr)
-	// 	readJson(numRows, rdr)
-	// }
+	defer f.Close()
+	runtime.GC()
+	pprof.WriteHeapProfile(f)
 
+	defer duration(track("Total execution time"))
+
+	// Обработка каждого файла в каталоге
+	for i := 1; i <= 11; i++ {
+		// Чтение набора данных из файла
+		numSets, rdr, outwrt, err := readSets(i)
+		if err != nil {
+			fmt.Println("Ошибка при чтении наборов данных:", err)
+			continue
+		}
+
+		// Обработка каждого набора данных
+		for j := 1; j <= numSets; j++ {
+			numRows := readRows(rdr)
+			readJson(numRows, rdr, outwrt)
+		}
+
+		// Сброс буфера для записи всех данных в файл
+		outwrt.Flush()
+	}
 }
 
-// Ф-я для чтения первого числа - кол-ва набора данных
-func readSets() (int, *bufio.Reader, error) {
-
-	//defer duration(track("readiInput"))
-
+// Функция для чтения первого числа - количества наборов данных
+func readSets(fileIndex int) (int, *bufio.Reader, *bufio.Writer, error) {
 	var numSets int
-
 	var rdr *bufio.Reader
 
-	// Цикл для обработки каждого файла в каталоге "71_5"
-	for i := 4; i <= 4; i++ {
-		iStr := strconv.Itoa(i)
-		// Путь до конкретного файла с наборами данных
-		filePathFull := fmt.Sprintf("%s%s", filePath, iStr)
+	// Формирование пути к конкретному файлу с наборами данных
+	iStr := strconv.Itoa(fileIndex)
+	filePathFull := fmt.Sprintf("%s%s", filePath, iStr)
 
-		// Открытие файла
-		file, err := os.Open(filePathFull)
-		if err != nil {
-			return 0, nil, fmt.Errorf("error opening the file: %w", err)
-		}
-		//defer file.Close() // Закрытие файла по окончании работы с ним
-
-		// Создание Reader для чтения файла
-		rdr = bufio.NewReader(file)
-
-		// Чтение количества наборов входных данных в файле
-		numOfSetsStr, err := rdr.ReadString('\n')
-		if err != nil {
-			return 0, nil, fmt.Errorf("Error reading amount of sets: %w", err)
-		}
-
-		// Преобразование количества наборов из строки в число
-		numOfSetsStr = strings.TrimSuffix(numOfSetsStr, "\n")
-		numOfSetsInt, err := strconv.Atoi(numOfSetsStr)
-		if err != nil {
-			return 0, nil, fmt.Errorf("failed to convert sets count to int: %w", err)
-		}
-
-		// Создание Writer для записи в соответ-й выходной файл
-		outWrite := bufio.NewWriter(writeToOutputFile(i))
-		defer outWrite.Flush()
-
-		// количество наборов входных данных
-		numSets = numOfSetsInt
-		// кол-во строк
-		// numRows = readRows(rdr)
+	// Открытие файла
+	file, err := os.Open(filePathFull)
+	if err != nil {
+		return 0, nil, nil, fmt.Errorf("error opening the file: %w", err)
 	}
-	return numSets, rdr, nil
+
+	// Создание Reader для чтения файла
+	rdr = bufio.NewReader(file)
+
+	// Чтение количества наборов входных данных в файле
+	numOfSetsStr, err := rdr.ReadString('\n')
+	if err != nil {
+		file.Close()
+		return 0, nil, nil, fmt.Errorf("Error reading amount of sets: %w", err)
+	}
+
+	// Преобразование количества наборов из строки в число
+	numOfSetsStr = strings.TrimSuffix(numOfSetsStr, "\n")
+	numOfSetsInt, err := strconv.Atoi(numOfSetsStr)
+	if err != nil {
+		file.Close()
+		return 0, nil, nil, fmt.Errorf("failed to convert sets count to int: %w", err)
+	}
+
+	// Создание Writer для записи в соответствующий выходной файл
+	outFile, err := writeToOutputFile(fileIndex)
+	if err != nil {
+		file.Close()
+		return 0, nil, nil, fmt.Errorf("failed to create output file: %w", err)
+	}
+
+	outWrite := bufio.NewWriter(outFile)
+
+	// Количество наборов входных данных
+	numSets = numOfSetsInt
+
+	return numSets, rdr, outWrite, nil
 }
 
-func readJson(numRows int, rdr *bufio.Reader) {
-	// Иниц-ия структуры чтобы избежать пустого указ-ля
+// Функция для чтения JSON строк и записи результатов
+func readJson(numRows int, rdr *bufio.Reader, outwrt *bufio.Writer) {
+	// Инициализация структуры для избежания пустого указателя
 	var root FoldersPath
 
-	// Чтение строк где содержится json
-
+	// Чтение строк с JSON данными
 	var jsonBuilder strings.Builder
 	for j := 1; j <= numRows; j++ {
 		line, err := rdr.ReadString('\n')
 		if err != nil {
 			fmt.Println("ошибка при чтении строки JSON:", err)
+			return
 		}
 		jsonBuilder.WriteString(line)
 	}
 
+	// Разбор JSON
 	if err := json.Unmarshal([]byte(jsonBuilder.String()), &root); err != nil {
-		fmt.Println("ошибка при разборе JSON: %w", err)
+		fmt.Println("ошибка при разборе JSON:", err)
+		return
 	}
 
+	// Поиск зараженных файлов
 	infected := make(map[string]bool)
 	findInfected(&root, "", infected)
 
-	// count := 0
-	// for path := range infected {
-	// 	fmt.Println(path)
-	// 	count++
-	// }
-	fmt.Printf("Total infected file and folders: %d\n", len(infected))
-	//fmt.Println(infected)
-
+	// Запись количества зараженных файлов в выходной файл
+	_, err := fmt.Fprintf(outwrt, "%d\n", len(infected))
+	if err != nil {
+		fmt.Println("ошибка при записи в файл:", err)
+	}
 }
 
-// Ф-я для чтения кол-ва строк с описанием директорий
+// Функция для чтения количества строк с описанием директорий
 func readRows(rdr *bufio.Reader) int {
 	strOfRow, err := rdr.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error in reading row with sets of rows", err)
+		return 0
 	}
 	strOfRows := strings.TrimSuffix(strOfRow, "\n")
 	numOfRows, err := strconv.Atoi(strOfRows)
 	if err != nil {
 		fmt.Println("Error in type converting", err)
+		return 0
 	}
 	return numOfRows
 }
 
+// Рекурсивная функция для поиска зараженных файлов и директорий
 func findInfected(node *FoldersPath, currentPath string, infected map[string]bool) {
 	fullPath := currentPath + "/" + node.Dir
 
@@ -178,8 +199,6 @@ func findInfected(node *FoldersPath, currentPath string, infected map[string]boo
 
 	// Если текущая директория заражена, все файлы и папки внутри нее заражены
 	if hasInfectedFile || isFolderInfected(node, fullPath, infected) {
-
-		//infected[fullPath] = true
 		for _, file := range node.Files {
 			infected[fullPath+"/"+file] = true
 		}
@@ -189,6 +208,7 @@ func findInfected(node *FoldersPath, currentPath string, infected map[string]boo
 	}
 }
 
+// Функция для проверки, является ли директория зараженной
 func isFolderInfected(node *FoldersPath, currentPath string, infected map[string]bool) bool {
 	fullPath := currentPath + "/" + node.Dir
 	if infected[fullPath] {
@@ -202,10 +222,10 @@ func isFolderInfected(node *FoldersPath, currentPath string, infected map[string
 	return false
 }
 
+// Рекурсивная функция для пометки всех файлов и папок внутри зараженной директории
 func markAllInfected(node *FoldersPath, currentPath string, infected map[string]bool) {
 	fullPath := currentPath + "/" + node.Dir
 
-	//infected[fullPath] = true
 	for _, file := range node.Files {
 		infected[fullPath+"/"+file] = true
 	}
